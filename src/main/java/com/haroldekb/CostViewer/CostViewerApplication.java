59d17b576ce348be26fd8ac01741206b4d22f282
haroldekb <haroldekb@mail.ru>
2020-03-16 01:45:14 +0500

    Initial commit

---
+++ src/main/java/com/haroldekb/CostViewer/CostViewerApplication.java
@@ -0,0 +1,13 @@
+package com.haroldekb.CostViewer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CostViewerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CostViewerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/haroldekb/CostViewer/CostViewerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.haroldekb.CostViewer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CostViewerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
